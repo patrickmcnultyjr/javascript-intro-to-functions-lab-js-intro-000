0166fdcc043cdd444713fde4d1d7db2ae06deb9d
Patrick McNulty <pamcnultyjr@gmail.com>
2020-02-05 14:21:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ var uppercase, lowercase
 
 function logshout(string){
   uppercase = string.toUpperCase();
-  string = uppercase;
   console.log (uppercase);
 }
-
 
 function shout(string){
   uppercase = string.toUpperCase();
