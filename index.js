47e115e518104169455015591db593db209ca055
Patrick McNulty <pamcnultyjr@gmail.com>
2020-02-05 16:45:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function shout(string){
   return string.toUpperCase();
 }
  function whisper(string){
-   return string.toLowerCase;
+   return string.toLowerCase();
  }
 function logShout(string){
   uppercase = string.toUpperCase()
