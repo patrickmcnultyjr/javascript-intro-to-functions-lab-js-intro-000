c8e37ae36a07d97e66792f73ee9e719a39bad67d
Patrick McNulty <pamcnultyjr@gmail.com>
2020-02-05 13:25:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,17 +2,3 @@
 function shout(string) {
   return string.toUpperCase()
 }
-
-function whisper(string) {
-  return (string).toLowerCase()
-}
-
-function sayHiToGrandma(string) {
-  if (string.lowercase === string) {
-    console.log "I can't hear you!";
-  } else if ((string) === (string).toUpperCase){
-    return "YES INDEED!";
-  } else if ((string) === "I love you, Grandma."){
-    return "I love you, too.";
-  }
-}
