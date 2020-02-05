10a6c9b8c0544f8517ad42e9abec4b61abcf6a26
Patrick McNulty <pamcnultyjr@gmail.com>
2020-02-05 13:09:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function sayHiToGrandma(string) {
   if ((string) === (string).toLowerCase) {
     return "I can't hear you!";
   } else if ((string) === (string).toUpperCase){
-    console.log "YES INDEED!";
+    return "YES INDEED!";
   } else if ((string) === "I love you, Grandma."){
     return "I love you, too.";
   }
